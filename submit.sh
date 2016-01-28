a080269f9276d52cd43d84988066cf19f94acbe4
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2016-01-28 19:09:12 +0900

    update submit.sh

--- submit.sh
+++ submit.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
+set -e
+
 cwd=$(cd `dirname $0`; pwd)
 
-for dir in *; do
-    [[ ! -d $cwd/$dir ]] && continue
-
-    cd $cwd/$dir
-    echo $dir
+if [[ $1 != "" ]]; then
+    cd $cwd/$1
     anaconda build submit --channel main --channel dev . || true
-done
+elif [[ $1 == "." ]]; then
+    anaconda build submit --channel main --channel dev . || true
+else
+    for dir in *; do
+        [[ ! -d $cwd/$dir ]] && continue
+
+        cd $cwd/$dir
+        echo $dir
+        anaconda build submit --channel main --channel dev . || true
+    done
+fi

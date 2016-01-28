8d584467c75ef6097de64fc777ce31d74c6e0672
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2016-01-29 00:54:47 +0900

    clean submit.sh

--- submit.sh
+++ submit.sh
@@ -7,8 +7,6 @@ cwd=$(cd `dirname $0`; pwd)
 if [[ $1 != "" ]]; then
     cd $cwd/$1
     anaconda build submit --channel main --channel dev . || true
-elif [[ $1 == "." ]]; then
-    anaconda build submit --channel main --channel dev . || true
 else
     for dir in *; do
         [[ ! -d $cwd/$dir ]] && continue

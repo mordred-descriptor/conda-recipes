783ab403f28b43a5dcc53ec9a82fa4602089cf5c
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2019-03-06 00:30:22 +0900

    wip

--- mopac7/before.sh
+++ mopac7/before.sh
@@ -3,7 +3,7 @@
 source ../common.sh
 
 if [[ "$OS_NAME" == Linux ]]; then
-    apt install -y gfortran-4.8
+    sudo apt install -y gfortran-4.8
 elif [[ "$OS_NAME" == MacOSX ]]; then
     brew install gcc
 fi

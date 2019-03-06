43e2916dc1e48d05412092336aa19aa10edc447f
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2019-03-06 13:49:40 +0900

    wip

--- mopac7/before.sh
+++ mopac7/before.sh
@@ -6,5 +6,11 @@ if [[ "$OS_NAME" == Linux ]]; then
     sudo apt update
     sudo apt install -y gfortran-4.8
 elif [[ "$OS_NAME" == MacOSX ]]; then
-    brew install -f gcc coreutils
+    for pkg in gcc coreutils; do
+        if brew list $pkg &> /dev/null; then
+	    brew upgrade $pkg || true
+	else
+	    brew install $pkg
+	fi
+    done
 fi
